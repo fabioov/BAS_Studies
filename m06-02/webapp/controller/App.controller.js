7129e809dde66613e8e4176aca5bca3c1241a117
Fabio Santos <fabioov@hotmail.com>
2023-02-06 13:20:02 -0300

    Folder

---
+++ m06-02/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("Fabio.m0602.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ m06-02/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Fabio/m0602/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
