b6cfe2e9d6b392894cc697332d60e04edd1f8003
Fabio Santos <fabioov@hotmail.com>
2023-02-10 00:09:23 +0000

    Deleted Folder

--- m06-02/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["Fabio/m0602/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- m06-02/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"Fabio/m06-02/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
